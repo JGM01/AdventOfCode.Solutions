925c44ff2c5337219819e1f13c7e9f450519a8c5
JGM01 <jgermana@students.kennesaw.edu>
2021-12-01 03:25:37 -0500

    I dont have much to say here lol this is just prep

---
+++ AdventOfCode.Solutions/2021/2021Day01.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Solutions._2021
+{
+    class _2021Day01 : Solver
+    {
+
+        public static int PartOne()
+        { 
+            var x = 0;
+            var l = ConvertToIntList(fileInput, x);
+
+            var bigger = 0;
+            for (int i = 1; i < l.Count; i++)
+                if (l[i] > l[i - 1])
+                    bigger++;
+
+            return bigger;
+
+        }
+
+        public static int PartTwo()
+        {
+            var x = 0;
+            var l = ConvertToIntList(fileInput, x);
+
+            var bigger = 0;
+            for (int i = 3; i < l.Count; i++)
+                if (l[i - 2] + l[i - 1] + l[i] > l[i - 3] + l[i - 2] + l[i - 1])
+                    bigger++;
+
+            return bigger;
+        }
+
+
+        public static List<int> ConvertToIntList(List<string> input, int x) => 
+            input.Where(c => int.TryParse(c, out x))
+                 .Select(c => x)
+                 .ToList();
+
+
+    }
+}
---
+++ AdventOfCode.Solutions/Solver.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode.Solutions._2021;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Solutions
+{
+    class Solver
+    {
+        public static readonly List<string> fileInput = new List<string>(File.ReadAllLines("A:/AOCINPUTS/day01.txt"));
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"DAY 1 PART 1: {_2021Day01.PartOne()}");
+            Console.WriteLine($"DAY 1 PART 2: {_2021Day01.PartTwo()}");
+
+        }
+    }
+}
