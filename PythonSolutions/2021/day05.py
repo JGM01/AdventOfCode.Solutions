8b675259c22a931c4b33906b2b59195b6b80b277
JGM01 <jacobgermanamccray@gmail.com>
2021-12-05 03:11:47 -0500

    Optimized py answer

--- PythonSolutions/2021/day05.py
+++ PythonSolutions/2021/day05.py
@@ -1,25 +1,7 @@
 import collections
 
 
-def part1(lines):
-    points = collections.defaultdict(int)
-    for line in lines.splitlines():
-        a, b = line.split("->")
-        x1, y1 = map(int, a.split(","))
-        x2, y2 = map(int, b.split(","))
-
-        if x1 != x2 and y1 != y2:
-            continue
-        if y1 == y2:
-            for i in range(min(x1, x2), max(x1, x2) + 1):
-                points[(i, y1)] += 1
-        if x1 == x2:
-            for i in range(min(y1, y2), max(y1, y2) + 1):
-                points[(x1, i)] += 1
-    print(sum(i > 1 for i in points.values()))
-
-
-def part2(lines):
+def day05(lines, includeHorizontal):
     points = collections.defaultdict(int)
 
     for line in lines.splitlines():
@@ -29,17 +11,21 @@ def part2(lines):
 
         if x1 != x2 and y1 != y2 and abs(x1 - x2) != abs(y1 - y2):
             continue
+
         if y1 == y2:
             for i in range(min(x1, x2), max(x1, x2) + 1):
                 points[(i, y1)] += 1
+
         if x1 == x2:
             for i in range(min(y1, y2), max(y1, y2) + 1):
                 points[(x1, i)] += 1
-        if abs(x1 - x2) == abs(y1 - y2):
-            y_mult = -1 if y1 > y2 else 1
-            x_mult = -1 if x1 > x2 else 1
-            for i in range(abs(x1 - x2) + 1):
-                points[(x1 + (x_mult * i), y1 + (y_mult * i))] += 1
+
+        if includeHorizontal:
+            if abs(x1 - x2) == abs(y1 - y2):
+                y_mult = -1 if y1 > y2 else 1
+                x_mult = -1 if x1 > x2 else 1
+                for i in range(abs(x1 - x2) + 1):
+                    points[(x1 + (x_mult * i), y1 + (y_mult * i))] += 1
 
     print(sum(i > 1 for i in points.values()))
 
@@ -47,5 +33,5 @@ def part2(lines):
 if __name__ == "__main__":
     with open("A:/AOCINPUTS/day05.txt") as f:
         data = f.read()
-    part1(data)
-    part2(data)
+    day05(data, False)
+    day05(data, True)
